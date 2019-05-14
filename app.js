9e65b0d76f694817e828cba86996015804b7512e
PeterChen123 <chenguanyu9745@outlook.com>
2019-05-14 15:27:55 -0700

    update the net app.js

--- app.js
+++ app.js
@@ -1,84 +1,55 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
 });
 
-var Campground = mongoose.model("Campground", campgroundSchema);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-// Campground.create(
-//     {
-//         name: "GIF",
-//         image: "https://i.pinimg.com/originals/cc/6e/dd/cc6edd8dd1998a1924aa9948bcf4a08a.gif"
-        
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         }
-//         else {
-//             console.log("Newly Created Campground");
-//             console.log(campground);
-//         }
-//     });
-
-
-
-// var campgrounds = [
-//     {name: "Chen", image: "https://i.pinimg.com/564x/03/0f/61/030f61c7d6f3edc33026974aae4d3f60.jpg"},
-//     {name: "GIF", image: "https://i.pinimg.com/originals/cc/6e/dd/cc6edd8dd1998a1924aa9948bcf4a08a.gif"}
-// ];
-
-app.get("/", function(req, res){
-    res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res){
-    //Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("campgrounds", {campgrounds:allCampgrounds});
-        }
-    });
-    
-    //res.render("campgrounds", {campgrounds:campgrounds});
-});
-
-app.post("/campgrounds", function(req, res){
-    //get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    //Create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.redirect("/campgrounds");
-        }
-    });
-    
-    //campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    //res.redirect("/campgrounds");
-});
-
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
-});
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Start the YelpCamp Server");
+   console.log("The YelpCamp Server Has Started!");
 });
